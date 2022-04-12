1df0c513322cc6db4920d11af0f24dc4938f11e0
pizihao <2335715300@qq.com>
2022-04-13 00:08:00 +0800

    Delete something and use default ThreadFactory

--- src/test/java/com/deep/crow/ThreadPool.java
+++ src/test/java/com/deep/crow/ThreadPool.java
@@ -1,8 +1,5 @@
 package com.deep.crow;
 
-import cn.hutool.core.thread.ExecutorBuilder;
-import cn.hutool.core.thread.NamedThreadFactory;
-
 import java.util.concurrent.*;
 
 /**
@@ -11,14 +8,12 @@ import java.util.concurrent.*;
 public class ThreadPool {
 
     public static ExecutorService executorService() {
-        ThreadFactory threadFactory = new NamedThreadFactory("Thread-", false);
         return new ThreadPoolExecutor(
             16,
             32,
             500,
             TimeUnit.SECONDS,
             new LinkedBlockingQueue<>(),
-            threadFactory,
             new ThreadPoolExecutor.AbortPolicy()
         );
     }
