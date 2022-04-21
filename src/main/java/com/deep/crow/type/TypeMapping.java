52e6bb5d229cc2258b064dbb7c6a17da135559a3
pizihao <2335715300@qq.com>
2022-04-21 20:27:29 +0800

    Matting matching pattern

---
+++ src/main/java/com/deep/crow/type/TypeMapping.java
@@ -0,0 +1,17 @@
+package com.deep.crow.type;
+
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
+
+/**
+ * <h2>类型匹配</h2>
+ * 一种模式匹配，以便于通过反射的方式匹配值的类型。<br>
+ *
+ * @author Create by liuwenhao on 2022/4/21 20:24
+ */
+public class TypeMapping {
+
+    Type type;
+
+
+}
