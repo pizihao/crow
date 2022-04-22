e808bf6e1eb2af227af921940f820e5761dd8ac6
pizihao <2335715300@qq.com>
2022-04-22 19:50:03 +0800

    ReflectUtil.java add screenType method

--- src/main/java/com/deep/crow/util/ReflectUtil.java
+++ src/main/java/com/deep/crow/util/ReflectUtil.java
@@ -1,5 +1,7 @@
 package com.deep.crow.util;
 
+import com.deep.crow.exception.CrowException;
+
 import java.util.List;
 
 /**
@@ -13,15 +15,24 @@ public class ReflectUtil {
     }
 
     /**
-     * <h2></h2>
+     * <h2>筛选结果类型</h2>
+     * 在一个未知的结果集中匹配指定类型的项，匹配成功则直接返回<br>
+     * 无匹配选项择抛出异常
      *
-     * @param l 结果集
-     * @param clazz
+     * @param l     结果集
+     * @param clazz 目标类
      * @return T
      * @author liuwenhao
      * @date 2022/4/22 19:33
      */
+    @SuppressWarnings("unchecked")
     public <T> T screenType(List<?> l, Class<T> clazz) {
-        return null;
+        for (Object o : l) {
+            boolean instance = clazz.isInstance(o);
+            if (instance) {
+                return (T) o;
+            }
+        }
+        throw CrowException.exception("无可匹配类型");
     }
 }
