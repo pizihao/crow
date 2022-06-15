1539872d27770b96ab949786be85cd5066499b51
pizihao <2335715300@qq.com>
2022-06-15 18:16:52 +0800

    How to write a mixed operation

--- src/main/java/com/deep/crow/task/mix/MixMulti.java
+++ src/main/java/com/deep/crow/task/mix/MixMulti.java
@@ -1,9 +1,14 @@
 package com.deep.crow.task.mix;
 
+import com.deep.crow.multi.Multi;
+
+import java.util.List;
+
 /**
  * <h2>混合执行过程</h2>
  *
  * @author Create by liuwenhao on 2022/6/14 15:59
  */
 public class MixMulti {
+    public List<Multi> multis;
 }
